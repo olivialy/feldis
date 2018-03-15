684e500112af9f7a9935a7874902b293e36c1fba
olivialy <olivia_ly@yahoo.fr>
2018-03-15 12:37:44 +0100

    Add markup and styles for top section

--- templates/_pages/_accueil.php
+++ templates/_pages/_accueil.php
@@ -1,8 +1,26 @@
-<p>lorem izebfizh iqzehf ieshc osjf oesijrf oseijf ozie oei jozie omseiru goeiru toi oeru</p>
-<p>lorem izebfizh iqzehf ieshc osjf oesijrf oseijf ozie oei jozie omseiru goeiru toi oeru</p>
-<p>lorem izebfizh iqzehf ieshc osjf oesijrf oseijf ozie oei jozie omseiru goeiru toi oeru</p>
-<p>lorem izebfizh iqzehf ieshc osjf oesijrf oseijf ozie oei jozie omseiru goeiru toi oeru</p>
-<p>lorem izebfizh iqzehf ieshc osjf oesijrf oseijf ozie oei jozie omseiru goeiru toi oeru</p>
-<p>lorem izebfizh iqzehf ieshc osjf oesijrf oseijf ozie oei jozie omseiru goeiru toi oeru</p>
-<p>lorem izebfizh iqzehf ieshc osjf oesijrf oseijf ozie oei jozie omseiru goeiru toi oeru</p>
-<p>lorem izebfizh iqzehf ieshc osjf oesijrf oseijf ozie oei jozie omseiru goeiru toi oeru</p>
+<!-- top section -->
+<section class="rTwins">
+    <div class="rTwins-child tHP-skills">
+        <div class="rTwins-child-content">
+            <h2>Ce que nous faisons <span>Nous le faisons bien</span></h2>
+            <p>Feldis & Leviaux est une entreprise générale de peinture, ravalement, revêtement de sol, vitrerie, menuiserie et plâtrerie, située à Melun, dans le sud de la Seine-et-Marne etse déplaçant dans toute l’Île-de-France.</p>
+            <a href="#" class="cta cta-pink">Découvrez nos compétences <svg height="30" width="30"><use xlink:href="#symbol-cta"></use></svg> </a>
+        </div>
+    </div>
+    <div class="rTwins-child">
+        <div class="tHP-certification">
+            <div class="rTwins-child-content">
+                <h2>Nos certifications</h2>
+                <p>Iso 9001 depuis 2017, notre rigueur porte ses fruits et nous permet de fournir un travail de qualité.</p>
+                <a href="#" class="cta cta-purple float-right">Découvrez nos certifications <svg height="30" width="30"><use xlink:href="#symbol-cta"></use></svg></a>
+            </div>
+        </div>
+        <div class="tHP-environment">
+            <div class="rTwins-child-content">
+                <h2>L'environnement</h2>
+                <p>Feldis & Leviaux s’engage à respecter une véritable démarche environnementale.</p>
+                <a href="#" class="cta cta-yellow float-right">Découvrez nos engagements <svg height="30" width="30"><use xlink:href="#symbol-cta"></use></svg></a>
+            </div>
+        </div>
+    </div>
+</section>
