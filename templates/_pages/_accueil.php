c9412d87c0f78c665186c78d8df2d60abbaa00f2
olivialy <olivia_ly@yahoo.fr>
2018-03-16 17:19:11 +0100

    Add markup and styles for 'newsletter' section

--- templates/_pages/_accueil.php
+++ templates/_pages/_accueil.php
@@ -1,6 +1,6 @@
 <!-- top section -->
 <section class="rTwins">
-    <div class="rTwins-child tHP-skills">
+    <div class="tHP-skills rTwins-child">
         <div class="rTwins-child-content">
             <h2>Ce que nous faisons <span>Nous le faisons bien</span></h2>
             <p>Feldis & Leviaux est une entreprise générale de peinture, ravalement, revêtement de sol, vitrerie, menuiserie et plâtrerie, située à Melun, dans le sud de la Seine-et-Marne etse déplaçant dans toute l’Île-de-France.</p>
@@ -144,3 +144,34 @@
         </div>
     </div>
 </section>
+
+
+<!-- newsletter -->
+<form action="/" class="tFormNewsletter bg-lightgrey pt3 pb1">
+    <div class="clearfix wrap">
+        <div class="tHP-newsletter float-left md-6 mb1">
+            <h3>Inscrivez-vous à notre newsletter</h3>
+            <p class="pb1">Pour connaître l’actualité de Feldis & Leviaux, nos événements, l’avancé de chantiers en Ile-de-France et à Paris, ainsiq eu l’actualité du secteur, nous vous proposons de recevoir tous les deux mois notre newsletter.</p>
+            <p><strong>Restez connecté à l’info et suivez de multiples projets !</strong></p>
+        </div>
+        <fieldset class="sm-12 md-5 md-offset-1 float-left">
+            <p>
+                <label class="sr-only" for="nl-name">Nom</label>
+                <input id="nl-name" type="text" placeholder="Nom" />
+            </p>
+            <p>
+                <label class="sr-only" for="nl-firstname">Prénom</label>
+                <input id="nl-firstname" type="text" placeholder="Prénom" />
+            </p>
+            <p>
+                <label class="sr-only" for="nl-company">Entreprise</label>
+                <input id="nl-company" type="text" placeholder="Entreprise" />
+            </p>
+            <p>
+                <label class="sr-only" for="nl-email">Email</label>
+                <input id="nl-email" type="email" placeholder="Email" />
+            </p>
+            <p class="align-center mt1 mb1"><input type="submit" class="cta" value="Valider"></p>
+        </fieldset>
+    </div>
+</form>
