9509deb3afd2f609614315e98e0707527ff518e2
olivialy <olivia_ly@yahoo.fr>
2018-03-16 18:07:57 +0100

    Add markup and styles for 'contact' section

--- templates/_pages/_accueil.php
+++ templates/_pages/_accueil.php
@@ -147,7 +147,7 @@
 
 
 <!-- newsletter -->
-<form action="/" class="tFormNewsletter bg-lightgrey pt3 pb1">
+<form action="/" class="tFormNewsletter pt3 pb1">
     <div class="clearfix wrap">
         <div class="tHP-newsletter float-left md-6 mb1">
             <h3>Inscrivez-vous à notre newsletter</h3>
@@ -172,6 +172,45 @@
                 <input id="nl-email" type="email" placeholder="Email" />
             </p>
             <p class="align-center mt1 mb1"><input type="submit" class="cta" value="Valider"></p>
+        </fieldset>
+    </div>
+</form>
+
+<!-- contact form -->
+<form action="/" class="tContact">
+    <div class="wrap pt3">
+        <h2 class="align-center">Contactez feldis & Leviaux</h2>
+        <fieldset class="md-6 md-offset-3">
+            <p>
+                <label class="sr-only" for="ct-name">Nom</label>
+                <input id="ct-name" type="text" placeholder="Votre nom*" required />
+            </p>
+            <p>
+                <label class="sr-only" for="ct-firstname">Prénom</label>
+                <input id="ct-firstname" type="text" placeholder="Votre prénom*" required />
+            </p>
+            <p>
+                <label class="sr-only" for="ct-fonction">Fonction</label>
+                <input id="ct-fonction" type="text" placeholder="Votre fonction" />
+            </p>
+            <p>
+                <label class="sr-only" for="ct-email">Email</label>
+                <input id="ct-email" type="email" placeholder="Votre email*" required />
+            </p>
+            <p>
+                <label class="sr-only" for="ct-tel">Téléphone</label>
+                <input id="ct-tel" type="text" placeholder="Votre téléphone" />
+            </p>
+            <p>
+                <label class="sr-only" for="ct-obj">Objet</label>
+                <input id="ct-obj" type="text" placeholder="Objet*" required />
+            </p>
+            <p class="pb0">
+                <label class="sr-only" for="ct-msg">Objet</label>
+                <textarea id="ct-msg" rows="6" placeholder="Votre message*" required></textarea>
+            </p>
+            <p class="color-pink"><em>*Champ obligatoire</em></p>
+            <p class="align-center mt1 mb1"><input type="submit" class="cta cta-pink" value="Valider"></p>
         </fieldset>
     </div>
 </form>
