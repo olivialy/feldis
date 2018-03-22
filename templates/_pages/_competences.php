e9c83fa8a4e374281c8850ba588deb45d3238248
olivialy <olivia_ly@yahoo.fr>
2018-03-22 11:08:49 +0100

    Add content and styles for 'rCover' region and 'sectionhead' component

--- templates/_pages/_competences.php
+++ templates/_pages/_competences.php
@@ -1,3 +1,11 @@
+<div class="rCover rCover-competences">
+    <div class="sectionhead">
+        <div class="wrap">
+            <h1 class="sectionhead-strongtitle">Des compétences au service <span>de tous vos projets</span></h1>
+            <p class="sectionhead-subheading wrap-small">Nos différentes formations, qualifications et certifications permettent à nos clients d’avoir une totale confiance dans nos process et notre savoir-faire. </p>
+        </div>
+    </div>
+</div>
 
 <!-- job section -->
 <?php include_once('templates/_components/_jobs.php'); ?>
